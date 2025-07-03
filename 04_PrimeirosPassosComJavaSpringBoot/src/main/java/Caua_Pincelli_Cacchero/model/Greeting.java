311d9aa7959428d3e206a9ea504d59f888c56428
Caua-Pincelli-Cacchero <cauapincacchero@gmail.com>
2025-07-03 15:59:16 -0300

    Organizando o projeto em diretórios

---
+++ 04_PrimeirosPassosComJavaSpringBoot/src/main/java/Caua_Pincelli_Cacchero/model/Greeting.java
@@ -0,0 +1,3 @@
+package 04_PrimeirosPassosComJavaSpringBoot.src.main.java.Caua_Pincelli_Cacchero.model;
+
+public record Greeting(long id, String content) {}
---
+++ 05_PathParams_TratamentoExcessões/src/test/java/Caua_Pincelli_Cacchero/StartupTests.java
@@ -0,0 +1,13 @@
+package Caua_Pincelli_Cacchero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
