d4e0965f632df6a30435aa4fe043cc1a7434a316
Caua-Pincelli-Cacchero <cauapincacchero@gmail.com>
2025-07-04 15:41:45 -0300

    Desafio_02

--- 04_PrimeirosPassosComJavaSpringBoot/src/main/java/Caua_Pincelli_Cacchero/model/Greeting.java
+++
@@ -1,3 +0,0 @@
-package 04_PrimeirosPassosComJavaSpringBoot.src.main.java.Caua_Pincelli_Cacchero.model;
-
-public record Greeting(long id, String content) {}
--- Principal/src/main/java/Caua_Pincelli_Cacchero/Startup.java
+++ Principal/src/main/java/Caua_Pincelli_Cacchero/Startup.java
@@ -1,4 +1,4 @@
-package 04_PrimeirosPassosComJavaSpringBoot.src.main.java.Caua_Pincelli_Cacchero;
+package Caua_Pincelli_Cacchero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
