8a212ff7f109ce4e57529d56ff4c0d3271af9b5c
Caua-Pincelli-Cacchero <cauapincacchero@gmail.com>
2025-07-21 19:03:42 -0300

    Definindo a Logica

---
+++ src/main/java/Caua_PIncelli_Cacchero/controllers/MathController.java
@@ -0,0 +1,29 @@
+package Caua_PIncelli_Cacchero.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String numberOne) {
+
+        return 1D;
+    }
+
+    private boolean isNumeric(String number) {
+        return true;
+    }
+}
