3cf32d8e15dfd24acbd96e6a3eb9ff763f336b43
Caua-Pincelli-Cacchero <cauapincacchero@gmail.com>
2025-07-24 12:01:49 -0300

    Retornando uma lista de objetos em JSON

--- src/main/java/Caua_PIncelli_Cacchero/PersonServices.java
+++ src/main/java/Caua_PIncelli_Cacchero/PersonServices.java
@@ -3,6 +3,8 @@ package Caua_PIncelli_Cacchero;
 import Caua_PIncelli_Cacchero.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,17 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one Person!");
 
@@ -20,6 +33,17 @@ public class PersonServices {
         person.setFirstName("Caua");
         person.setLastName("Cacchero");
         person.setAddress("Sao Paulo");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some address in Brazil");
         person.setGender("Male");
 
         return person;
